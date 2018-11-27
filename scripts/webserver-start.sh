b051746570fb2f986fb040b470c3a437afd4237e
laurentbusecke <45357232+laurentbusecke@users.noreply.github.com>
2018-11-27 14:13:46 +0100

    Add files via upload

--- scripts/webserver-start.sh
+++ scripts/webserver-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#/etc/init/etc/init.d/httpd start
 sudo /etc/init.d/httpd start
+#sudo systemctl start httpd
 
 
--- scripts/webserver-stop.sh
+++ scripts/webserver-stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo /etc/init.d/httpd stop
-
+# sudo systemctl stop httpd
 
